08a66a9f0e459b3457d9c13709118326941453ff
princepaswan67 <sprincepaswan3478@gmail.com>
2025-05-03 12:22:12 +0530

    Fix GitHub Pages deployment

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 console.log('Lets write JavaScript');
 console.log("hello");
 
-let songs = [
-    "song1.mp3",
-    "song2.mp3",
-    "song3.mp3"
-];
+let currentSong = new Audio();
+
+const playMusic = (track) => {
+    currentSong.src = "songs/" + track;
+    currentSong.play().then(() => {
+        console.log("Playing:", track);
+        play.src = "pause.svg";
+        document.querySelector(".songinfo").innerHTML = track;
+        document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
+    }).catch((err) => {
+        console.error("Playback error:", err);
+    });
+};
 
 async function main() {
-    // Use the predefined array
-    // let songs = await getSongs()  <-- remove this
+    // Hardcoded song list
+    let songs = [
+        "song1.mp3",
+        "song2.mp3",
+        "song3.mp3"
+    ];
 
-    // song list you define manually
-    let songs = ["song1.mp3", "song2.mp3"]; // update this list with your actual filenames
-
-    let songUL = document.querySelector(".songlist ul");
+    // Show songs in playlist
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0];
     for (const song of songs) {
-        songUL.innerHTML += `
-            <li>
-                <img src="music.svg" alt="">
-                <div class="info">
-                    <div>${song.replaceAll("%20", " ")}</div>
-                    <div>Song Artist</div>
-                </div>
-                <div class="playnow">
-                    <span>Play Now</span>
-                    <img src="play.svg" alt="">
-                </div>
-            </li>
-        `;
+        songUL.innerHTML += `<li> 
+            <img src="music.svg" alt="">
+            <div class="info">
+                <div>${song.replaceAll("%20", " ")}</div>
+                <div>Song Artist</div>
+            </div>
+            <div class="playnow">
+                <span>Play Now</span>
+                <img class="" src="play.svg" alt="">
+            </div>
+        </li>`;
     }
 
-    Array.from(songUL.getElementsByTagName("li")).forEach((e) => {
+    // Add click listeners to each song
+    Array.from(document.querySelector(".songlist").getElementsByTagName("li")).forEach(e => {
         e.addEventListener("click", () => {
-            let track = e.querySelector(".info").firstElementChild.innerHTML.trim();
-            playMusic(track);
+            let trackName = e.querySelector(".info").firstElementChild.innerHTML.trim();
+            playMusic(trackName);
         });
     });
 
+    // Play/pause button
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play();
@@ -48,6 +58,7 @@ async function main() {
         }
     });
 
+    // Update progress bar
     currentSong.addEventListener("timeupdate", () => {
         document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration) * 100 + "%";
     });
